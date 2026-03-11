6a00b01bc686ef2ba7f900324433bb3ab9bdbc1f
mitnick <brunovega1985@gmail.com>
2026-03-11 12:36:00 +0100

    mejorando el archivo de categorues/index.blade.php para  añadir las clases de Bootstrap a la tabla con borde .
     mejorar diseño, tambien añadir botones de editar , eliminar con clases. para futuro de JS.
    
     centrar tambien el titulo

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -1,29 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>***Listado de Categorías***</h1>
-    <a href="{{ route('categories.create') }}">Crear Nueva Categoría</a>
-    <table class="table table-striped" border="1">
-        <thead>
+    <h1 class="text-center my-4">***Listado de Categorías***</h1>
+    <a href="{{ route('categories.create') }}" class="btn btn-success mb-3">Crear Nueva Categoría</a>
+    <table class="table table-striped table-bordered">
+        <thead class="table-dark">
             <tr>
-                <th>ID</th>
-                <th>Nombre</th>
-                <th>Descripción</th>
-                <th>Acciones</th>
+                <th class="text-center">ID</th>
+                <th class="text-center">Nombre</th>
+                <th class="text-center">Descripción</th>
+                <th class="text-center">Acciones</th>
             </tr>
         </thead>
         <tbody>
             @foreach ($categories as $category)
             <tr>
-                <td>{{ $category->id }}</td>
+                <td class="text-center">{{ $category->id }}</td>
                 <td>{{ $category->name }}</td>
                 <td>{{ $category->description }}</td>
-                <td>
-                    <a href="{{ route('categories.edit', $category->id) }}">Editar</a>
+                <td class="text-center">
+                    <a href="{{ route('categories.edit', $category->id) }}" class="btn btn-primary btn-sm">Editar</a>
                     <form action="{{ route('categories.destroy', $category->id) }}" method="POST" style="display:inline;">
                         @csrf
                         @method('DELETE')
-                        <button type="submit">Eliminar</button>
+                        <button type="submit" class="btn btn-danger btn-sm">Eliminar</button>
                     </form>
                 </td>
             </tr>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Funko Football Collection')</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
     <header>
